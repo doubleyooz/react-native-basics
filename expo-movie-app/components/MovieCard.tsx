fdb8556b8a12f22d253ae4b860a47872e940227e
Joanderson <sirjoandersonwf@gmail.com>
2025-03-12 16:45:57 -0300

    feat: movie card component

---
+++ expo-movie-app/components/MovieCard.tsx
@@ -0,0 +1,36 @@
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+import { icons } from '@/constants/icons';
+
+const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
+  return (
+    <Link href={`/movie/${id}`}>
+      <TouchableOpacity>
+        <Image
+          source={{
+            uri: poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'https://placeholder.co/600x400/1a1a1a/ffffff.png',
+          }}
+          className="w-full h-52 rounded-lg"
+          resizeMode="cover"
+        />
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">{Math.round(vote_average * 10) / 10}</Text>
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium uppercase">{release_date?.split('-')[0]}</Text>
+          <Text className="text-xs text-light-300 font-medium uppercase">Movie</Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+export default MovieCard;
